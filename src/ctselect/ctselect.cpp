b67cd9ee05d2b8ad3374bd7c5af29279e2c91c94
knodlseder <>
2010-05-12 13:48:21 +0000

    initial dummy versions

---
+++ src/ctselect/ctselect.cpp
@@ -0,0 +1,40 @@
+/***************************************************************************
+ *                                  ctselect                               *
+ * ----------------------------------------------------------------------- *
+ *  copyright (C) 2010 by Jurgen Knodlseder                                *
+ * ----------------------------------------------------------------------- *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/**
+ * @file ctselect.cpp
+ * @brief CTA data selection tool
+ * @author J. Knodlseder
+ */
+
+/* __ Includes ___________________________________________________________ */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+#include <stdio.h>
+#include "ctselect.hpp"
+
+
+/***********************************************************************//**
+ * @brief Entry point
+ *
+ * @param[in] argc Number of arguments
+ * @param[in] argv Arguments
+ ***************************************************************************/
+int main (int argc, char *argv[])
+{
+    // Dummy
+    printf("Hello world!\n");
+
+    // Return
+    return 0;
+}
---
+++ src/ctselect/ctselect.hpp
@@ -0,0 +1,30 @@
+/***************************************************************************
+ *                                  ctselect                               *
+ * ----------------------------------------------------------------------- *
+ *  copyright (C) 2010 by Jurgen Knodlseder                                *
+ * ----------------------------------------------------------------------- *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/**
+ * @file ctselect.hpp
+ * @brief CTA data selection tool definition
+ * @author J. Knodlseder
+ */
+
+#ifndef CTSELECT_HPP
+#define CTSELECT_HPP
+
+/* __ Includes ___________________________________________________________ */
+
+/* __ Namespaces _________________________________________________________ */
+
+/* __ Constants __________________________________________________________ */
+
+/* __ Prototypes ________________________________________________________ */
+
+#endif /* CTSELECT_HPP */
