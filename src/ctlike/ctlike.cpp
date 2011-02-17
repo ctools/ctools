b8655e158bf39456ca54c8a4d22ca6f44788bcbd
knodlseder <>
2011-02-17 23:03:06 +0000

    set maximum number of stalls to 5

--- src/ctlike/ctlike.cpp
+++ src/ctlike/ctlike.cpp
@@ -489,7 +489,7 @@ void ctlike::init_members(void)
     m_obs.clear();
     m_refit     = false;
     m_max_iter  = 100;   // Set maximum number of iterations
-    m_max_stall = 10;    // Set maximum number of stalls
+    m_max_stall = 5;     // Set maximum number of stalls
     m_logL      = 0.0;
     m_opt       = NULL;
 
