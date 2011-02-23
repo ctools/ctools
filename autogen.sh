b67d5ae7265455763bae59501d0a08fd4826b437
knodlseder <>
2011-02-23 23:51:27 +0000

    optionally use either libtoolize or glibtoolize

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
 aclocal -I m4
-libtoolize --copy
+if which libtoolize >/dev/null; then
+  libtoolize --copy
+else
+  if which glibtoolize >/dev/null; then
+    glibtoolize --copy
+  fi
+fi
+#libtoolize --copy
 autoconf
 autoheader
 automake --add-missing --copy
