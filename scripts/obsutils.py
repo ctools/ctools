2fbc6670313913763c525ab5a09e8dd259c2417b
Hubert Siejkowski <h.siejkowski@cyfronet.pl>
2014-07-10 16:03:53 +0200

    Add an depracted obsutils.py in its previous location

---
+++ scripts/obsutils.py
@@ -0,0 +1,5 @@
+print "The obsutils are moved under ctools module."
+print "Please update your srcipt to import from ctools, i.e:"
+print "from ctools import obsutils"
+
+from ctools.obsutils import *
