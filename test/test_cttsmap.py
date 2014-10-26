924e3120ba66c151e5a97a65426b1a551c6affe7
Jürgen Knödlseder <jurgen.knodlseder@irap.omp.eu>
2014-10-26 22:52:40 +0100

    Remove debugging.

--- test/test_cttsmap.py
+++ test/test_cttsmap.py
@@ -67,7 +67,6 @@ class Test(gammalib.GPythonTestSuite):
         """
         # Set-up cttsmap
         tsmap = ctools.cttsmap()
-        tsmap["debug"].boolean(True)
         tsmap["infile"].filename(self.events_name)
         tsmap["srcmdl"].filename(self.model_name)
         tsmap["srcname"].string("Crab")
