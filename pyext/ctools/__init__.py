e8d48779e4d7844cbc59be9c72f2070de939111c
Jürgen Knödlseder <jurgen.knodlseder@irap.omp.eu>
2014-10-27 18:34:18 +0100

    Remove unnecessary modules

--- pyext/ctools/__init__.py
+++ pyext/ctools/__init__.py
@@ -1,3 +1,9 @@
 from .ctools import *
 import obsutils
 
+bad_entries = [entry for entry in list(locals())
+               if entry.endswith('_swigregister')]
+for entry in bad_entries:
+    del locals()[entry]
+del gammalib
+del ctools
