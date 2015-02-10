26d34cb1dbb475752a2c4cbaaf72604d9e302c1b
Jürgen Knödlseder <jurgen.knodlseder@irap.omp.eu>
2015-02-10 13:47:04 +0100

    Improve spectral plotting (automatic dynamic range adjustment)

--- examples/show_model.py
+++ examples/show_model.py
@@ -20,6 +20,7 @@
 # ==========================================================================
 import gammalib
 import sys
+import math
 try:
     import matplotlib.pyplot as plt
 except:
@@ -41,21 +42,36 @@ def plot_spectrum(model, emin=0.01, emax=100.0, enumbins=100):
     ebounds = gammalib.GEbounds(enumbins, e_min, e_max)
 
     # Setup model plot
-    x = []
-    y = []
+    x   = []
+    y   = []
+    min = 1.0e30
+    max = 0.0
     for i in range(enumbins):
         energy = ebounds.elogmean(i)
+        value  = spectrum.eval(energy, gammalib.GTime())
+        if value > max:
+            max = value
+        if value < min:
+            min = value
         x.append(energy.TeV())
-        y.append(spectrum.eval(energy, gammalib.GTime()))
+        y.append(value)
+
+    # Set minimum and maximum
+    max = math.pow(10.0, math.log10(max)+1)
+    if min < 0.0001*max:
+        min = 0.0001*max
+    min = math.pow(10.0, math.log10(min)-1)
+    
 
     # Show spectrum
     plt.figure()
-    plt.title(model.name())
+    plt.title(model.name()+" ("+spectrum.type()+")")
     plt.loglog()
     plt.grid()
     plt.loglog(x, y, color='red')
     plt.xlabel("Energy [TeV]")
-    plt.ylabel(r"dN/dE [ph s$^{-1}$ cm$^{-2}$ MeV$^{-1}$]")    
+    plt.ylabel(r"dN/dE [ph s$^{-1}$ cm$^{-2}$ MeV$^{-1}$]")
+    plt.ylim([min,max]) 
     plt.show()
     
     # Return
