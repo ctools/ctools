19945a1249fe9eaa937a5a286ee209005a7b541c
Jürgen Knödlseder <jurgen.knodlseder@irap.omp.eu>
2015-07-02 12:59:03 +0200

    Change the bkgcube parameter to "f" type (was "s" type)

--- scripts/csresmap.py
+++ scripts/csresmap.py
@@ -107,7 +107,7 @@ class csresmap(ctools.cscript):
             pars.append(gammalib.GApplicationPar("modcube","f","a","NONE","","","Model cube file (generated with ctmodel)"))            
             pars.append(gammalib.GApplicationPar("expcube","f","a","NONE","","","Exposure cube file (only needed for stacked analysis)"))
             pars.append(gammalib.GApplicationPar("psfcube","f","a","NONE","","","PSF cube file (only needed for stacked analysis)"))
-            pars.append(gammalib.GApplicationPar("bkgcube","s","a","NONE","","","Background cube file (only needed for stacked analysis)"))
+            pars.append(gammalib.GApplicationPar("bkgcube","f","a","NONE","","","Background cube file (only needed for stacked analysis)"))
             pars.append(gammalib.GApplicationPar("inmodel","f","a","$CTOOLS/share/models/crab.xml","","","Source model"))
             pars.append(gammalib.GApplicationPar("outmap","f","a","resmap.fits","","","Output residual map"))
             pars.append(gammalib.GApplicationPar("caldb","s","a","prod2","","","Calibration database"))
