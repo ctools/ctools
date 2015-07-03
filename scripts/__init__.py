d131d288d34f7081bb8333c0a8a9b068da8fa8b3
Jürgen Knödlseder <jurgen.knodlseder@irap.omp.eu>
2015-07-03 12:48:37 +0200

    Move obsutils into cscripts module.

---
+++ scripts/__init__.py
@@ -0,0 +1,25 @@
+# List all scripts in this module
+__all__ = ["cscaldb", \
+           "cshessobs", \
+           "csinfo", \
+           "cslightcrv", \
+           "csobsdef", \
+           "cspull", \
+           "csresmap", \
+           "cssens", \
+           "csspec", \
+           "cstsdist", \
+           "obsutils"]
+
+# Import
+from .cscaldb    import cscaldb
+from .cshessobs  import cshessobs
+from .csinfo     import csinfo
+from .cslightcrv import cslightcrv
+from .csobsdef   import csobsdef
+from .cspull     import cspull
+from .csresmap   import csresmap
+from .cssens     import cssens
+from .csspec     import csspec
+from .cstsdist   import cstsdist
+import obsutils
