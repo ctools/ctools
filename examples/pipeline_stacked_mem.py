77f1091b9c2457bd0b93a57fd37cd185cd6e8a80
Jürgen Knödlseder <jurgen.knodlseder@irap.omp.eu>
2016-08-01 23:11:55 +0200

    Reduce duration of unit test

--- examples/pipeline_stacked_mem.py
+++ examples/pipeline_stacked_mem.py
@@ -178,4 +178,4 @@ if __name__ == '__main__':
     obs.models(gammalib.GModels(datadir+'/crab.xml'))
 
     # Run analysis pipeline
-    run_pipeline(obs)
+    run_pipeline(obs, enumbins=10, nxpix=40, nypix=40, binsz=0.1)
