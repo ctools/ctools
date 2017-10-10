b0f2c752434fea48ddc3e1824024a6182b130908
Luigi Tibaldo <luigi.tibaldo@irap.omp.eu>
2017-10-10 12:17:04 +0200

    script skeleton

--- cscripts/csphagen.py
+++ cscripts/csphagen.py
@@ -19,5 +19,130 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # ==========================================================================
-import sys
 import gammalib
+import ctools
+
+
+# =============== #
+# csfindobs class #
+# =============== #
+class csphagen(ctools.cscript):
+    """
+    Generate pha files for classical IACT spectral analysis
+    """
+
+    # Constructor
+    def __init__(self, *argv):
+        """
+        Constructor
+        """
+        # Set name, version
+        self._name = 'csfindobs'
+        self._version = ctools.__version__
+
+        # Initialise observation container from constructor arguments
+        self._obs, argv = self._set_input_obs(argv)
+
+        # Initialise application by calling the appropriate class constructor
+        self._init_cscript(argv)
+
+        # Return
+        return
+
+    # Private methods
+    def _get_parameters(self):
+        """
+        Get parameters from parfile and setup observations
+        """
+        # If there are no observations in container then query the inobs
+        # parameter
+        if self._obs.size() == 0:
+            self['inobs'].filename()
+
+        # Query energy binning parameters
+        self['ebinalg'].string()
+        if self['ebinalg'].string() == 'FILE':
+            self['ebinfile'].filename()
+        else:
+            self['emin'].real()
+            self['emax'].real()
+            self['enumbins'].integer()
+
+        # Initialise background estimation method
+        self._bkgmethod = self["bkgmethod"].string()
+
+        # Initialise source position/region querying relevant parameters
+        self._src_dir = gammalib.GSkyDir()
+        self._src_reg = gammalib.GSkyRegions()
+        coordsys = self['coordsys'].string()
+        if coordsys == "CEL":
+            ra = self['ra'].real()
+            dec = self['dec'].real()
+            self._src_dir.radec_deg(ra, dec)
+        elif coordsys == "GAL":
+            glon = self['glon'].real()
+            glat = self['glat'].real()
+            self._src_dir.lb_deg(glon, glat)
+        self._srcshape = self['srcshape'].string()
+        if self._srcshape == "CIRCLE":
+            self._rad = self['rad'].real()
+            self._src_reg.append(
+                gammalib.GSkyRegionCircle(self._src_dir, self._rad))
+
+        # Stacking
+        self._stack = self['stack'].bool()
+
+        # Query ahead output parameters
+        if (self._read_ahead()):
+            self['outroot'].string()
+
+        # Write input parameters into logger
+        self._log_parameters(gammalib.TERSE)
+
+        # Return
+        return
+
+    def _reflected_regions(self, obs):
+        """
+        Calculate list of reflected regions for a single observation
+        :param obs: observation
+        :return: list of reflected regions
+        """
+        pnt_dir = obs.pointing().dir()
+        offset = pnt_dir.dist_deg(self._src_dir)
+        outregions = []
+
+        return outregions
+
+    # Public methods
+    def run(self):
+        # Switch screen logging on in debug mode
+        if self._logDebug():
+            self._log.cout(True)
+
+        # Write observation into logger
+        self._log_observations(gammalib.NORMAL, self._obs, 'Observation')
+
+        # Set energy binning
+        if self['ebinalg'] == "FILE":
+            pass
+            ## need to load binning from file
+        else:
+            etrue = gammalib.GEbounds(self["enumbins"],
+                                      gammalib.GEnergy(self["emin"], 'TeV'),
+                                      gammalib.GEnergy(self["emax"], 'TeV'))
+            ereco = gammalib.GEbounds(self["enumbins"],
+                                      gammalib.GEnergy(self["emin"], 'TeV'),
+                                      gammalib.GEnergy(self["emax"], 'TeV'))
+
+        # Loop through observations and generate pha, arf, rmf files
+        outobs = gammalib.GObservations()
+        for obs in self._obs:
+            bkg_reg = gammalib.GSkyRegions()
+            if self._bkgmethod == "REFLECTED":
+                regions = self.reflected_regions(obs)
+            for region in regions:
+                bkg_reg.append(region)
+            onoff = gammalib.GCTAOnOffObservation(obs, etrue, ereco,
+                                                  self._src_region, bkg_reg)
+            outobs.append(onoff)
