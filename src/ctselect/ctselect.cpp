9ecb561a32a6ac4a0bc524459dd3c0fcb2bf0b93
Jürgen Knödlseder <jurgen.knodlseder@irap.omp.eu>
2018-01-22 17:35:20 +0100

    Update date

--- src/ctselect/ctselect.cpp
+++ src/ctselect/ctselect.cpp
@@ -1,7 +1,7 @@
 /***************************************************************************
  *                      ctselect - Data selection tool                     *
  * ----------------------------------------------------------------------- *
- *  copyright (C) 2010-2017 by Juergen Knoedlseder                         *
+ *  copyright (C) 2010-2018 by Juergen Knoedlseder                         *
  * ----------------------------------------------------------------------- *
  *                                                                         *
  *  This program is free software: you can redistribute it and/or modify   *
